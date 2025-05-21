ab5252239836872e873f0ff1b73da0a022829cc3
Polinaaaaaaaaaaaaaaaaaaaaaa <pdavydova967@gmail.com>
2025-05-21 08:16:15 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,1 +1,33 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
 
+#include <stdio.h>
+
+// Структура записи файла
+typedef struct {
+    char *path;
+    char *content;
+} FileEntry;
+
+// Структура файловой системы
+typedef struct {
+    FileEntry *entries;
+    size_t count;
+} FileSystem;
+
+// Открытие файла для чтения и записи
+int open_file(const char *filename, FILE **fptr);
+
+// Просмотр содержимого файла по имени
+int view_file(const char *target, char **buffer);
+
+// Создание нового файла в файловой системе
+int fs_create(FileSystem *fs, const char *path, const char *data);
+
+// Удаление файла по пути
+int fs_delete(FileSystem *fs, const char *path);
+
+// Обновление содержимого файла
+int fs_update(FileSystem *fs, const char *path, const char *new_data);
+
+#endif // FILESYSTEM_H
